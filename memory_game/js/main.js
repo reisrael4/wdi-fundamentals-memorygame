31f991b1fbac6daba717c920517545999d68ba49
Rachel Israel <rachelnims@Rachels-MacBook.fios-router.home>
2020-01-11 23:29:10 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+const cards = ['queen' , 'queen' , 'king' , 'king'];
+const cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('User flipped queen');
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log('User flipped king');
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo ) {
+		alert('You found a match!');
+	}
+	else {
+		alert('Sorry, try again.');
+	}
+}
