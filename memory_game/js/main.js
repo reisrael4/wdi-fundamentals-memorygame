b530a143944d994022d9becafca38736170abeb8
Rachel Israel <rachelnims@Rachels-MacBook.fios-router.home>
2020-01-12 23:41:41 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,46 @@
-const cards = ['queen' , 'queen' , 'king' , 'king'];
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diammonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diammonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+]
 
-const cardsInPlay = [];
+var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1] ) {
-		console.log('You found a match!');
+		if (cardsInPlay[0] === cardsInPlay[1] ) {
+				alert('You found a match!');
 		}
-	else {
-		console.log('Sorry, try again.');
+			else {
+				alert('Sorry, try again.');
 		}
 	}
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
-};
+}
 
 flipCard(0);
 flipCard(2);
